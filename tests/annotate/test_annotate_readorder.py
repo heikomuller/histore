d2c40175860447b051a6192fb4db2514b7f7e51f
Heiko Mueller <heiko.muller@gmail.com>
2020-05-12 16:43:00 -0400

    Add unit tests for multi-column primary keys

--- tests/annotate/test_annotate_readorder.py
+++ tests/annotate/test_annotate_readorder.py
@@ -9,6 +9,8 @@
 index values.
 """
 
+import pytest
+
 import histore.annotate.base as annotate
 
 
@@ -16,15 +18,17 @@ def test_primarykey_keys():
     """Test getting key values for a document that is keyed by a list of
     primary key columns.
     """
+    rows=[
+        [1, 'Alice'],
+        [2, 1.3],
+        ['Claire', 3],
+        [4, None],
+        [5, ['Eve']]
+    ]
+    # Single column key
     readorder = annotate.pk_readorder(
         columns=['ID', 'Name'],
-        rows=[
-            [1, 'Alice'],
-            [2, 1.3],
-            ['Claire', 3],
-            [4, None],
-            [5, ['Eve']]
-        ],
+        rows=rows,
         primary_key='Name'
     )
     assert len(readorder) == 5
@@ -37,6 +41,46 @@ def test_primarykey_keys():
     assert readorder[2][1].is_number()
     assert readorder[3][1].is_null()
     assert readorder[4][1].is_string()
+    # Multi column key
+    readorder = annotate.pk_readorder(
+        columns=['ID', 'Name'],
+        rows=rows,
+        primary_key=['Name', 'ID']
+    )
+    assert len(readorder) == 5
+    for i in range(5):
+        rowidx, _, pos = readorder[i]
+        assert rowidx == i
+        assert pos == i
+    assert readorder[0][1][0].is_string()
+    assert readorder[0][1][1].is_number()
+    assert readorder[1][1][0].is_number()
+    assert readorder[1][1][1].is_number()
+    assert readorder[2][1][0].is_number()
+    assert readorder[2][1][1].is_string()
+    assert readorder[3][1][0].is_null()
+    assert readorder[3][1][1].is_number()
+    assert readorder[4][1][0].is_string()
+    assert readorder[4][1][1].is_number()
+    # Error cases
+    with pytest.raises(ValueError):
+        annotate.pk_readorder(
+            columns=['SSN', 'Name'],
+            rows=rows,
+            primary_key=['Name', 'ID']
+        )
+    with pytest.raises(ValueError):
+        annotate.pk_readorder(
+            columns=['Name', 'SSN', 'Name', 'Name'],
+            rows=rows,
+            primary_key=['Name']
+        )
+    # No error if duplicate column is not part of the primary key.
+    annotate.pk_readorder(
+        columns=['ID', 'Name', 'SSN', 'SSN'],
+        rows=rows,
+        primary_key=['Name']
+    )
 
 
 def test_rowindex_keys():
